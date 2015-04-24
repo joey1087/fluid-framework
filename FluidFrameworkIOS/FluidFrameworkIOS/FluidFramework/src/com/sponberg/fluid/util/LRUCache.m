9a5dd4bb997b8b1506341782c250f9650fcb43b2
joey1087 <joeynguyen1087@gmail.com>
2015-04-24 16:32:36 +1000

    iOS translated files

--- FluidFrameworkIOS/FluidFrameworkIOS/FluidFramework/src/com/sponberg/fluid/util/LRUCache.m
+++ FluidFrameworkIOS/FluidFrameworkIOS/FluidFramework/src/com/sponberg/fluid/util/LRUCache.m
@@ -3,7 +3,11 @@
 //  source: src-delomboked/com/sponberg/fluid/util/LRUCache.java
 //
 
+#include "com/sponberg/fluid/FluidApp.h"
+#include "com/sponberg/fluid/GlobalState.h"
+#include "com/sponberg/fluid/SystemService.h"
 #include "com/sponberg/fluid/util/LRUCache.h"
+#include "java/lang/Runnable.h"
 #include "java/util/ArrayList.h"
 #include "java/util/HashMap.h"
 #include "java/util/Map.h"
@@ -21,9 +25,8 @@
 - (BOOL)removeEldestEntryWithJavaUtilMap_Entry:(id<JavaUtilMap_Entry>)eldest {
   BOOL shouldRemove = [self size] > maxEntries_;
   if (shouldRemove) {
-    for (id<FFTLRUCache_RemovedListener> __strong l in nil_chk(removedListeners_)) {
-      [((id<FFTLRUCache_RemovedListener>) nil_chk(l)) entryWasRemovedWithId:[((id<JavaUtilMap_Entry>) nil_chk(eldest)) getKey] withId:[eldest getValue]];
-    }
+    id<JavaLangRunnable> r = [[FFTLRUCache_$1 alloc] initWithFFTLRUCache:self withJavaUtilMap_Entry:eldest];
+    [((id<FFTSystemService>) nil_chk([((FFTFluidApp *) nil_chk(FFTGlobalState_get_fluidApp__())) getSystemService])) runOnUiThreadWithJavaLangRunnable:r];
   }
   return [self size] > maxEntries_;
 }
@@ -66,6 +69,36 @@
   };
   static J2ObjcClassInfo _FFTLRUCache_RemovedListener = { "RemovedListener", "com.sponberg.fluid.util", "LRUCache", 0x209, 1, methods, 0, NULL, 0, NULL};
   return &_FFTLRUCache_RemovedListener;
+}
+
+@end
+
+@implementation FFTLRUCache_$1
+
+- (void)run {
+  for (id<FFTLRUCache_RemovedListener> __strong l in nil_chk(this$0_->removedListeners_)) {
+    [((id<FFTLRUCache_RemovedListener>) nil_chk(l)) entryWasRemovedWithId:[((id<JavaUtilMap_Entry>) nil_chk(val$eldest_)) getKey] withId:[val$eldest_ getValue]];
+  }
+}
+
+- (id)initWithFFTLRUCache:(FFTLRUCache *)outer$
+    withJavaUtilMap_Entry:(id<JavaUtilMap_Entry>)capture$0 {
+  this$0_ = outer$;
+  val$eldest_ = capture$0;
+  return [super init];
+}
+
++ (J2ObjcClassInfo *)__metadata {
+  static J2ObjcMethodInfo methods[] = {
+    { "run", NULL, "V", 0x1, NULL },
+    { "initWithFFTLRUCache:withJavaUtilMap_Entry:", "init", NULL, 0x0, NULL },
+  };
+  static J2ObjcFieldInfo fields[] = {
+    { "this$0_", NULL, 0x1012, "Lcom.sponberg.fluid.util.LRUCache;", NULL,  },
+    { "val$eldest_", NULL, 0x1012, "Ljava.util.Map$Entry;", NULL,  },
+  };
+  static J2ObjcClassInfo _FFTLRUCache_$1 = { "$1", "com.sponberg.fluid.util", "LRUCache", 0x8000, 2, methods, 2, fields, 0, NULL};
+  return &_FFTLRUCache_$1;
 }
 
 @end
