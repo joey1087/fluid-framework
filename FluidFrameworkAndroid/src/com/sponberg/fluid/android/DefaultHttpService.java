c5cadaa3c078cc0a84a372b10fb273bc8f69044e
James Hong <yoonseokedev@gmail.com>
2015-09-10 15:32:09 +1000

    - implemented http post request with multipart body - added necessary jar files for multipart request

--- FluidFrameworkAndroid/src/com/sponberg/fluid/android/DefaultHttpService.java
+++ FluidFrameworkAndroid/src/com/sponberg/fluid/android/DefaultHttpService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
@@ -15,13 +16,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
 import com.sponberg.fluid.GlobalState;
@@ -35,6 +40,7 @@ public class DefaultHttpService implements HttpService {
 	enum HttpMethod { get, post, put };
 	
 	public DefaultHttpService() {
+		
 		HttpParams httpParameters = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParameters, 3000);
 		HttpConnectionParams.setSoTimeout(httpParameters, 10000);
@@ -44,62 +50,67 @@ public class DefaultHttpService implements HttpService {
 	public void get(String url, HashMap<String, Object> properties,
 			HttpAuthorization auth, HttpServiceCallback callback) {
 
-		startHttpRequest(properties, auth, callback, HttpMethod.get, url, false);
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.get, url);
+		config.properties = properties;
+		startHttpRequest(config);
 	}
 
 	@Override
 	public void getBinary(String url, HashMap<String, Object> properties,
 			HttpAuthorization auth, HttpServiceCallback callback) {
 
-		startHttpRequest(properties, auth, callback, HttpMethod.get, url, true);
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.get, url);
+		config.properties = properties;
+		config.binary = true;
+		startHttpRequest(config);
 	}
 
 	@Override
 	public void post(String url, HashMap<String, Object> properties,
 			HttpAuthorization auth, HttpServiceCallback callback) {
 		
-		startHttpRequest(properties, auth, callback, HttpMethod.post, url, false);
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.post, url);
+		config.properties = properties;
+		startHttpRequest(config);
+	}
+
+	@Override
+	public void post(String url, HashMap<String, Object> properties, PostBodyType postBodyType, HttpAuthorization auth, HttpServiceCallback callback) {
+		
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.post, url);
+		config.properties = properties;
+		config.isMultipart = true;
+		startHttpRequest(config);
 	}
 
 	@Override
 	public void postRaw(String url, String rawPost, HttpAuthorization auth,
 			HttpServiceCallback callback) {
 		
-		startHttpRequest(rawPost, auth, callback, HttpMethod.post, url, false);
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.post, url);
+		config.rawPost = rawPost;
+		startHttpRequest(config);
 	}
 	
 	@Override
 	public void put(String url, HashMap<String, Object> properties,
 			HttpAuthorization auth, HttpServiceCallback callback) {
 		
-		startHttpRequest(properties, auth, callback, HttpMethod.put, url, false);
+		HttpRequestConfiguration config = new HttpRequestConfiguration(auth, callback, HttpMethod.put, url);
+		config.properties = properties;
+		startHttpRequest(config);
 	}
-
-	protected void startHttpRequest(final HashMap<String, Object> properties,
-				final HttpAuthorization auth, final HttpServiceCallback callback, 
-				final HttpMethod httpMethod, final String url, final boolean binary) {
-		
-		startHttpRequest(null, properties, auth, callback, httpMethod, url, binary);
-	}
-	protected void startHttpRequest(final String rawPost,
-			final HttpAuthorization auth, final HttpServiceCallback callback, 
-			final HttpMethod httpMethod, final String url, final boolean binary) {
-		
-		startHttpRequest(rawPost, null, auth, callback, httpMethod, url, binary);
-	}
-
-	protected void startHttpRequest(final String rawPost, final HashMap<String, Object> properties,
-			final HttpAuthorization auth, final HttpServiceCallback callback, 
-			final HttpMethod httpMethod, final String url, final boolean binary) {
+	
+	protected void startHttpRequest(final HttpRequestConfiguration config) {
 
 		if (GlobalState.fluidApp.getSystemService().isOnUiThread()) {
 			
-			new RequestTask(rawPost, properties, auth, callback, httpMethod, binary).execute(url);
+			new RequestTask(config.rawPost, config.properties, config.auth, config.callback, config.httpMethod, config.isMultipart, config.binary).execute(config.url);
 		} else {
 			
 			Runnable r = new Runnable() {
 				public void run() {
-					new RequestTask(rawPost, properties, auth, callback, httpMethod, binary).execute(url);
+					new RequestTask(config.rawPost, config.properties, config.auth, config.callback, config.httpMethod, config.isMultipart, config.binary).execute(config.url);
 				}
 			};
 			GlobalState.fluidApp.getSystemService().runOnUiThread(r);
@@ -119,6 +130,8 @@ public class DefaultHttpService implements HttpService {
 
 		final HttpMethod httpMethod;
 		
+		final boolean isMultipart;
+		
 		final boolean binary;
 		
 		boolean success = false;
@@ -126,35 +139,64 @@ public class DefaultHttpService implements HttpService {
 		int statusCode = -1;
 		
 		public RequestTask(String rawPost, HashMap<String, Object> properties,
-				HttpAuthorization auth, HttpServiceCallback callback, HttpMethod httpMethod,
-				boolean binary) {
+				HttpAuthorization auth, HttpServiceCallback callback, HttpMethod httpMethod, boolean isMultipart, boolean binary) {
 
 			this.rawPost = rawPost;
 			this.properties = properties;
 			this.auth = auth;
 			this.callback = callback;
 			this.httpMethod = httpMethod;
+			this.isMultipart = isMultipart;
 			this.binary = binary;
 		}
 
 		@Override
 		protected String doInBackground(String... uri) {
+			
 			HttpClient httpclient = new DefaultHttpClient();
-			HttpResponse response;
+			HttpResponse response = null;
 			String responseString = null;
 			try {
-				
 				if (httpMethod == HttpMethod.post) {
-					HttpPost post = new HttpPost(uri[0]);
-					if (rawPost != null) {
-						post.setEntity(new StringEntity(rawPost));	
-					} else if (properties != null) {
-						ArrayList<NameValuePair> pairs = new ArrayList<>();
-						for (String key : properties.keySet())
-							pairs.add(new BasicNameValuePair(key, properties.get(key).toString()));
-						post.setEntity(new UrlEncodedFormEntity(pairs));
+					if (isMultipart) {
+						try {
+							HttpPost post = new HttpPost(uri[0]);
+							MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+							entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+							HttpEntity entity = entityBuilder.build();
+		
+							if (properties != null) {
+								for (String key : properties.keySet()) {
+									Object value = properties.get(key);
+									if (value instanceof String || value instanceof Number) {
+										entityBuilder.addTextBody(key, String.valueOf(value));
+									} else if (value instanceof Bitmap) {
+										ByteArrayOutputStream baos = new ByteArrayOutputStream();
+							            ((Bitmap)value).compress(Bitmap.CompressFormat.PNG, 100, baos);
+							            byte[] imageBytes = baos.toByteArray();
+										entityBuilder.addBinaryBody(key, imageBytes, ContentType.create("image/jpeg"), null);
+									}
+								}
+								post.setEntity(entity);
+							}
+							
+						    response = httpclient.execute(post);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					} else {
+						HttpPost post = new HttpPost(uri[0]);
+						if (rawPost != null) {
+							post.setEntity(new StringEntity(rawPost));	
+						} else if (properties != null) {
+							ArrayList<NameValuePair> pairs = new ArrayList<>();
+							for (String key : properties.keySet())
+								pairs.add(new BasicNameValuePair(key, properties.get(key).toString()));
+							post.setEntity(new UrlEncodedFormEntity(pairs));
+						}
+						
+						response = httpclient.execute(post);
 					}
-					response = httpclient.execute(post);
 				} else if (httpMethod == HttpMethod.put) {
 					HttpPut put = new HttpPut(uri[0]);
 					if (properties != null) {
@@ -163,6 +205,7 @@ public class DefaultHttpService implements HttpService {
 							pairs.add(new BasicNameValuePair(key, properties.get(key).toString()));
 						put.setEntity(new UrlEncodedFormEntity(pairs));
 					}
+					
 					response = httpclient.execute(put);					
 				} else {
 					String paramString = uri[0];
@@ -172,6 +215,7 @@ public class DefaultHttpService implements HttpService {
 							pairs.add(new BasicNameValuePair(key, properties.get(key).toString()));
 						paramString += "?" + URLEncodedUtils.format(pairs, "utf-8");
 					}
+					
 					response = httpclient.execute(new HttpGet(paramString));					
 				}
 				
@@ -199,18 +243,31 @@ public class DefaultHttpService implements HttpService {
 
 		@Override
 		protected void onPostExecute(String result) {
+			
 			if (success)
 				callback.success(new HttpServiceCallback.HttpResponse(result, statusCode));
 			else
 				callback.fail(new HttpServiceCallback.HttpResponse(result, statusCode));
 		}
 	}
-
-	@Override
-	public void post(String arg0, HashMap<String, Object> arg1,
-			PostBodyType arg2, HttpAuthorization arg3, HttpServiceCallback arg4) {
-		// TODO Auto-generated method stub
+	
+	private class HttpRequestConfiguration {
 		
+		public String rawPost = null;
+		public HashMap<String, Object> properties = null;
+		public HttpAuthorization auth = null;
+		public HttpServiceCallback callback = null;
+		public HttpMethod httpMethod = HttpMethod.get;
+		public boolean isMultipart = false;
+		public String url = null;
+		public boolean binary = false;
+		
+		public HttpRequestConfiguration (HttpAuthorization auth, HttpServiceCallback callback, HttpMethod httpMethod, String url) {
+			
+			this.auth = auth;
+			this.callback = callback;
+			this.httpMethod = httpMethod;
+			this.url = url;
+		}
 	}
-
 }
